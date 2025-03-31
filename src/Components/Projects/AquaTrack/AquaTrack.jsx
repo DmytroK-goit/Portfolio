08cc103c7259b28c176ac65a840ef3c8b75bdab1
Dmytro Kovbasiuk <k0vbasyuk.dim0n@gmail.com>
2025-03-31 21:52:34 +0300

    work with individual project style

--- src/Components/Projects/AquaTrack/AquaTrack.jsx
+++ src/Components/Projects/AquaTrack/AquaTrack.jsx
@@ -3,50 +3,32 @@ import { FaHandPointRight } from "react-icons/fa";
 import head from "../../../images/AquaTrack/head.jpg";
 import mainPage from "../../../images/AquaTrack/mainPage.jpg";
 import settings from "../../../images/AquaTrack/settings.jpg";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/effect-flip";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-import { EffectFlip, Pagination, Navigation } from "swiper/modules";
+
 export const AquaTrack = () => {
   return (
     <div className={s.container}>
-      <h3 className={s.title}>AquaTrack</h3>
       <ul className={s.list}>
-        <Swiper
-          effect={"flip"}
-          grabCursor={true}
-          pagination={true}
-          navigation={true}
-          modules={[EffectFlip, Pagination, Navigation]}
-          className="mySwiperPhoto"
-        >
-          <SwiperSlide>
-            <li>
-              <img src={head} alt="Head page" />
-            </li>
-          </SwiperSlide>
-          <SwiperSlide>
-            <li>
-              <img src={mainPage} alt="mainPage" />
-            </li>
-          </SwiperSlide>
-          <SwiperSlide>
-            <li>
-              <img src={settings} alt="settings" />
-            </li>
-          </SwiperSlide>
-        </Swiper>
+        <h3 className={s.title}>AquaTrack</h3>
+        <li>
+          <img src={head} alt="Head page" />
+        </li>
+        <li>
+          <img src={mainPage} alt="mainPage" />
+        </li>
+        <li>
+          <img src={settings} alt="settings" />
+        </li>
       </ul>
-      <p className={s.parag}>
-        Team project. The Aqua Track application is a hydration tracking tool
-        designed to help users monitor and improve their daily water intake. As
-        the Front-End Team Lead for the Aqua Track project, I led the
-        development of the user interface using React, managed the application's
-        state with Redux, and provided support to the development team. Server
-        part is developed with Mongo DB.
-      </p>
+      <div>
+        <p className={s.parag}>
+          Team project. The Aqua Track application is a hydration tracking tool
+          designed to help users monitor and improve their daily water intake.
+          As the Front-End Team Lead for the Aqua Track project, I led the
+          development of the user interface using React, managed the
+          application's state with Redux, and provided support to the
+          development team. Server part is developed with Mongo DB.
+        </p>
+      </div>
       <div className={s.cont_button}>
         <p>PRESS</p>
         <FaHandPointRight />
