b62580e9f246062802c3127a996021d48942ee8e
Dmytro Kovbasiuk <159040488+DmytroK-goit@users.noreply.github.com>
2026-01-18 16:43:32 +0200

    add translate to 404 page

--- src/Components/NotFound/NotFound.jsx
+++ src/Components/NotFound/NotFound.jsx
@@ -1,9 +1,11 @@
+import { useTranslation } from "react-i18next";
 import s from "./NotFound.module.scss";
 const NotFound = () => {
+  const { t } = useTranslation();
   return (
     <div className={s.cont}>
       <a className={s.home_button} href="/">
-        Додому
+        {t("notFound.home")}
       </a>
     </div>
   );
